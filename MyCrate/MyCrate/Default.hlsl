0bba3e1166c46cd67b261f092eb1c7a4330ce572
HollowEmiya <73419954+HollowEmiya@users.noreply.github.com>
2022-05-08 14:00:41 +0800

    Crate for Try Tex

---
+++ MyCrate/MyCrate/Default.hlsl
@@ -0,0 +1,107 @@
+#ifndef NUM_DIR_LIGHT
+    #define NUM_DIR_LIGHT 3
+#endif
+
+#ifndef NUM_POINT_LIGHT
+    #define NUM_POINT_LIGHT 0
+#endif
+
+#ifndef NUM_SPOT_LIGHT
+    #define NUM_SPOT_LIGHT 0
+#endif
+
+#include "../../Shader/LightingUtil.hlsl"
+
+Texture2D gDiffuseMap : register(t0);
+SamplerState gsamLinear : register(s0);
+
+cbuffer cbPerObject : register(b0)
+{
+    float4x4 gWorld;
+    float4x4 gTexTransform;
+}
+
+cbuffer cbPass : register(b1)
+{
+    float4x4 gView;
+    float4x4 gInvView;
+    float4x4 gProj;
+    float4x4 gInvProj;
+    float4x4 gViewProj;
+    float4x4 gInvViewProj;
+    float3 gEyePosW;
+    float cbPerObjectPad1;
+    float2 gRenderTargetSize;
+    float2 gInvRenderTargetSize;
+    float gNearZ;
+    float gFarZ;
+    float gToTalTime;
+    float gDeltaTime;
+    float4 gAmbientLight;
+    Light gLights[MaxLights];
+};
+
+cbuffer cbMaterial : register(b2)
+{
+    float4 gDiffuseAlbedo;
+    float3 gFresnelR0;
+    float gRoughness;
+    float4x4 gMatTransform;
+};
+
+struct VerrtexIn
+{
+    float3 PosL : POSITION;
+    float3 Normal : NORMAL;
+    float2 TexC : TEXCOORD;
+};
+
+struct VertexOut
+{
+    float4 PosH : SV_POSITION;
+    float3 PosW : POSITION;
+    float3 NormalW : NORMAL;
+    float2 TexC : TEXCOORD;
+};
+
+VertexOut VS(VerrtexIn vin)
+{
+    VertexOut vout = (VertexOut) 0.0f;
+    
+    float4 posW = mul((vin.PosL, 1.0f), gWorld);
+    vout.PosW = posW;
+    
+    vout.NormalW = mul(vin.Normal, (float3x3) gWorld);
+    
+    vout.PosH = mul(posW, gViewProj);
+    
+    // 输出经过三角形插值的顶点属性？
+    // Output vertex attributes for interpolation across triangle.
+    float4 texC = mul(float4(vin.TexC, 0.0f, 1.0f), gTexTransform);
+    vout.TexC = mul(texC, gMatTransform).xy;
+    
+    return vout;
+}
+
+float4 PS(VertexOut pin):SV_Target
+{
+    float4 diffuseAbledo = gDiffuseAlbedo * gDiffuseMap.Sample(gsamLinear, pin.TexC);
+    
+    pin.NormalW = normalize(pin.NormalW);
+    
+    float3 toEyeW = normalize(gEyePosW - pin.PosW);
+    
+    float4 ambient = diffuseAbledo * gAmbientLight;
+    
+    const float shininess = 1.0f - gRoughness;
+    Material mat = { diffuseAbledo, gFresnelR0, shininess };
+    float3 shadowFactor = 1.0f;
+    float4 directLight = ComputeLighting(gLights, mat, pin.PosW,
+        pin.NormalW, toEyeW, shadowFactor);
+
+    float4 litColor = ambient + directLight;
+    
+    litColor.a = diffuseAbledo.a;
+    
+    return litColor;
+}
