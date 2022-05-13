8e8c9298e952a0bb8ab4aa27487c1200dcc9105a
HollowEmiya <73419954+HollowEmiya@users.noreply.github.com>
2022-05-13 09:43:22 +0800

    Complete Waves

---
+++ BlendDemo/BlendDemo/BlendApp.cpp
@@ -0,0 +1,17 @@
+#include "../../Common/d3dApp.h"
+#include "../../Common/MathHelper.h"
+#include "../../Common/UploadBuffer.h"
+#include "../../Common/GeometryGenerator.h"
+#include "FrameResource.h"
+#include "Waves.h"
+
+using Microsoft::WRL::ComPtr;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+const int gNumFrameResources = 3;
+
+struct RenderItem
+{
+	RenderItem() = default;
+};
