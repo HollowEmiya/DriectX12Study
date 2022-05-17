3db9905476f4a7144368cf51512791e7ba97ce91
HollowEmiya <73419954+HollowEmiya@users.noreply.github.com>
2022-05-17 12:12:11 +0800

    FrameResource

---
+++ StencilDemo/StencilDemo/StencilApp.cpp
@@ -0,0 +1,33 @@
+#include "../../Common/d3dApp.h"
+#include "../../Common/MathHelper.h"
+#include "../../Common/UploadBuffer.h"
+#include "../../Common/GeometryGenerator.h"
+#include "FrameResource.h"
+
+using Microsoft::WRL::ComPtr;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+const int gNumFrameResources = 3;
+
+struct RenderItem
+{
+	RenderItem() = default;
+
+	XMFLOAT4X4 World = MathHelper::Identity4x4();
+
+	XMFLOAT4X4 TexTransform = MathHelper::Identity4x4();
+
+	int NumFrmaeDirety = gNumFrameResources;
+
+	UINT ObjCBIndex = -1;
+
+	Material* Mat = nullptr;
+	MeshGeometry* Geo = nullptr;
+
+	D3D12_PRIMITIVE_TOPOLOGY PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+
+	UINT IndexCount = 0;
+	UINT StartIndexLocation = 0;
+	int BaseVertexLocation = 0;
+};
